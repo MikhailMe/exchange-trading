ebba764c926bea320a6a1eb6e441e199d53cf090
MikhailMe <neon-pf@yandex.ru>
2019-03-27 03:22:55 +0300

    created spring boot project

---
+++ src/main/java/com/kspt/exchangetrading/ExchangeTradingApplication.java
@@ -0,0 +1,13 @@
+package com.kspt.exchangetrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeTradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kspt/exchangetrading/ExchangeTradingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kspt.exchangetrading;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExchangeTradingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
